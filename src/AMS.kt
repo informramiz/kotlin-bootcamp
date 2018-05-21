6b993ea802f656e1c5dcc09f5b6c3591ed844913
Ramiz Raja <informramiz@gmail.com>
2018-05-21 16:54:15 +0500

    feat: Print user's fortune based on birthday

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,7 +1,25 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    feedTheFish()
+    println(getFortuneCookie())
+}
+
+fun getFortuneCookie(): String {
+    val fortunes = listOf(
+            "You will have a great day!",
+            "Things will go well for you today.",
+            "Enjoy a wonderful day of success.",
+            "Be humble and all will turn out well.",
+            "Today is a good day for exercising restraint.",
+            "Take it easy and enjoy life!",
+            "Treasure your friends because they are your greatest fortune."
+    )
+
+    print("Enter your birthday: ")
+    val birthdayStr = readLine()
+    val birthday = birthdayStr?.toIntOrNull() ?: 1
+    val remainder = birthday % fortunes.size
+    return fortunes[remainder]
 }
 
 fun feedTheFish() {
