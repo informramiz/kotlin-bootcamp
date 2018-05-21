bc735743035194149aedf6bec48aa4326cd9e425
Ramiz Raja <informramiz@gmail.com>
2018-05-21 16:15:35 +0500

    feat: Print current day

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,3 +1,4 @@
+import java.util.*
 
 fun main(args: Array<String>) {
     dayOfWeek()
@@ -5,4 +6,17 @@ fun main(args: Array<String>) {
 
 fun dayOfWeek() {
     println("What day is it today?")
+    val dayOfWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+    val currentDay = when(dayOfWeek) {
+        Calendar.SUNDAY -> "Sunday"
+        Calendar.MONDAY -> "Monday"
+        Calendar.TUESDAY -> "Tuesday"
+        Calendar.WEDNESDAY -> "Wednesday"
+        Calendar.THURSDAY -> "Thursday"
+        Calendar.FRIDAY -> "Friday"
+        Calendar.SATURDAY -> "Saturday"
+        else -> "Sunday"
+    }
+
+    println("Today is $currentDay")
 }
