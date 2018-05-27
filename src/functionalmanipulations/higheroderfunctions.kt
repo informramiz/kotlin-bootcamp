1a08fd0ce76423f8f26f140e80869289bc005fbe
Ramiz Raja <informramiz@gmail.com>
2018-05-27 16:38:17 +0500

    feat: Try higher order functions let, run, apply

--- src/functionalmanipulations/higheroderfunctions.kt
+++ src/functionalmanipulations/higheroderfunctions.kt
@@ -1,12 +1,27 @@
 package functionalmanipulations
 
+data class Fish(var name: String)
+
 fun main(args: Array<String>) {
-    var name: String = "ramiz"
-    name = myWith(name) {
+    var myName: String = "ramiz"
+    myName = myWith(myName) {
         capitalize()
     }
+    println(myName)
 
-    println(name)
+    //try run higher order function: returns result of execution of lambda
+    val fish = Fish("splashy")
+    println(fish.run { name })
+
+    //try apply: returns object on which it is applied, after applying the lambda
+    val fish2 = Fish("splashy").apply { name = "sharky" }
+    println(fish2.name)
+
+    //try let: returns the copy of the changed object
+    println(fish.let { it.name.capitalize() }
+            .let { it + " fish" }
+            .let { it.length}
+            .let { it + 31 })
 }
 
 fun myWith(name: String, block: String.() -> String): String {
